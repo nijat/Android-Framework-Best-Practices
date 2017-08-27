dc19a03ee84e319f17bbf7e9006d1a60f05f069e
Nicat Rzayev <rzaev.nicat@gmail.com>
2017-08-27 13:48:43 +0400

    Update ApiClient.java

--- Retrofit Example/ApiClient.java
+++ Retrofit Example/ApiClient.java
@@ -1,1 +1,24 @@
-{\rtf1}
+public class ApiClient {
+
+    public static String BASE_URL = "http://api.example.com/";
+
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+        if (retrofit==null) {
+            OkHttpClient okHttpClient = new OkHttpClient().newBuilder()
+                    .connectTimeout(60, TimeUnit.SECONDS)
+                    .readTimeout(60, TimeUnit.SECONDS)
+                    .writeTimeout(60, TimeUnit.SECONDS)
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+
+}
